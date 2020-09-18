94af0504f670624c5b5e6779c680434a1d2b4084
Cansu35 <71334287+Cansu35@users.noreply.github.com>
2020-09-18 07:41:24 +0200

    Create Question3.java

---
+++ Question3.java
@@ -0,0 +1,21 @@
+package day_2;
+
+public class Question3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Kevin");
+		System.out.println("Mike");
+		System.out.println("David");
+		System.out.println("Christina");
+		System.out.println("Martha");
+		System.out.println("Raquel");
+		System.out.println("Merlin");
+		System.out.println("Adrian");
+		System.out.println("Alba");
+		System.out.println("Carolina");
+
+	}
+
+}
